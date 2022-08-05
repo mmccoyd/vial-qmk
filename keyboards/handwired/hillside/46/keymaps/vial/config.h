48c4b084bbaf043d94f13a25abc8e2a52982d07f
mmccoyd <mmccoyd@cs.berkley.edu>
2022-08-05 14:12:26 -0700

    46: Rename via/ to vial/. Add vial specifics.

---
+++ keyboards/handwired/hillside/46/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8F, 0xFB, 0xBB, 0xDD, 0x5D, 0x6E, 0x29, 0x95}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 5 }
